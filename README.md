fff3ce0cd7f0d2993cee92a050d26666264e1c5e
jahnavisai03 <jahnavisai2002@gmail.com>
2024-06-05 15:49:25 +0530

    run the pipeline

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OLIST-customer-data-MLOPS
 MLOPS Pipeline using ZenML
+
+pip install 'zenml["server"]'
+zenml init
+// if any error : zenml downgrade
+zenml up
---
+++ run_pipeline.py
@@ -0,0 +1,4 @@
+from pipelines.training_pipeline import train_pipeline
+
+if __name__ == "__main__":
+    train_pipeline(data_path=r'C:\Users\jahna\OneDrive\Desktop\OLIST-customer-data-MLOPS\data\olist_customers_dataset.csv')
